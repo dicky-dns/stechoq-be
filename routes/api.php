f5f28f6faeb4197a563b5bda47894c6af682e9a2
Dicky Dwi Nur Setyo <dickydns1@gmail.com>
2026-01-29 13:58:08 +0700

    feat: delete endpoint report

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IssueController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +13,6 @@ Route::as('api.')->group(function (): void {
         Route::post('/auth/logout', [AuthController::class, 'logout']);
         Route::apiResource('/projects', ProjectController::class)->except(['destroy']);
         Route::apiResource('/issues', IssueController::class)->except(['destroy']);
-        Route::apiResource('/reports', ReportController::class)->only(['index']);
         Route::apiResource('/users', UserController::class)->only(['index']);
     });
 });
